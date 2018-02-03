7ca37fc76dee67b79d2a93839a570c11a7ce63f3
Yağız Gazibaba <yagiz@Yagzs-MacBook-Air.local>
2018-02-04 01:40:53 +0300

    Initial Commit

---
+++ faceDetect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faceDetect
+//
+//  Created by Yağız Gazibaba on 04/02/2018.
+//  Copyright © 2018 Yağız Gazibaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
